f4e5beafa5b6445498c7500b79d8d0c45e620aee
GopalDas <gopalkrdas1997@gmail.com>
2022-05-29 04:10:56 +0530

    ADD: Checking two line on the basis of their points

--- src/com/bridgeLabz/LineComparison.java
+++ src/com/bridgeLabz/LineComparison.java
@@ -23,5 +23,19 @@ public class LineComparison {
         length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         System.out.println("Distance between two point in line1 is:-  " + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + "):- " + length1);
         Double line1 = Double.valueOf(length1);
+
+        System.out.println("Enter x3 point:- ");
+        x3 = scanner.nextInt();
+        System.out.println("Enter y3 point:- ");
+        y3 = scanner.nextInt();
+        System.out.println("Enter x4 point:- ");
+        x4 = scanner.nextInt();
+        System.out.println("Enter y4 point");
+        y4 = scanner.nextInt();
+
+        length2 = Math.sqrt(Math.pow(x4-x3,2) + Math.pow(y4-y3,2));
+        System.out.println("Distance between two point in line2 is:-  "+"("+x3+","+y3+"),"+"("+x4+","+y4+"):- "+length2);
+        Double line2 = Double.valueOf(length2);
+
     }
 }
