3137902e092174b130f340d1b8dd8499a7f0757f
GopalDas <gopalkrdas1997@gmail.com>
2022-05-29 04:01:24 +0530

    Add:- Displaying welcome line

---
+++ src/com/bridgeLabz/LineComparison.java
@@ -0,0 +1,10 @@
+package com.bridgeLabz;
+
+import java.util.Scanner;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome Line comparison computation program on master branch ");
+
+    }
+}
